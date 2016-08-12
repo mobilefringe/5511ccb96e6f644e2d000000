60c62d3a9a42c00abc47020c9e4c6d8a5675144f
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2016-08-12 08:03:09 -0400

    Merivale Mall 0.1  /survey.js

--- survey.js
+++ survey.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     // var contest = "twinpines-contest-2";
     var contest = "merivale-survey";
     
-    $('form[name="survey_form"]').validator().on('submit', function(e){
+    $('form[name="survey_form"]').on('submit', function(e){
         e.preventDefault();
         
         var data = {};
