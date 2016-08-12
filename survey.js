8895e05da6c8f6cf1c8f2509bd66a1499ee297b5
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2016-08-12 07:55:39 -0400

    Merivale Mall 0.1  /survey.js

---
+++ survey.js
@@ -0,0 +1,40 @@
+
+// var host = "http://twinpines.lvh.me:3000/";
+var host = "https://twinpines.mallmaverickstaging.com/";
+// var host = "http://merivale.mallmaverick.com"
+// var contest = "twinpines-contest-2";
+var contest = "merivale-survey";
+
+$('form').on('submit', function(e){
+    e.preventDefault();
+    
+    var data = {};
+    data['json'] = JSON.stringify($('form[name="survey_form"]').serializeArray());
+    submitContest(data);
+});
+
+function isValidForm() {
+    return true;
+}
+
+function submitContest(data) {
+    if (!isValidForm(data)) {
+        return false;
+    };
+    
+    var propertyDetails = getPropertyDetails();
+    // var host = propertyDetails.mm_host
+    
+    $.ajax({
+        url: [host, "contests", contest, "json_entry"].join("/"),
+        type: "POST",
+        dataType: "json",
+        data: data,
+        success: function(data) {
+            window.location.href="/survey_thank_you";
+        },
+        error: function(data){
+            alert("There was an issue with submitting the contest entry. Please try again at a later time.")
+        }
+    });
+}
