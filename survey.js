9e2fa90bcddd63a815910c37451c6479edf89e19
shinda@mobilefringe.com @ CodeCloud <shinda@mobilefringe.com @ CodeCloud>
2016-08-12 10:36:20 -0400

    Merivale Mall 0.1  /survey.js

--- survey.js
+++ survey.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
     });
     
     $('#surveyProgress input').on('change', function() {
+       
         updateProgress();
     });
 
@@ -42,54 +43,55 @@ $(document).ready(function(){
     }
 
 });
-
-    function updateProgress() {
-        var container = $('#surveyProgress');
+    
+/** This method isn't optimized but serves the purpoose for this form. */
+function updateProgress() {
+    var container = $('#surveyProgress');
+    
+    var totalQuestions = $('#surveyQuestions .form-group').length;
+    var totalAnswered = 0;
+    
+    // Loop over all our questions if a value is entered for any of our fields.
+    // mark it as an asnwered question
+    $('#surveyQuestions .form-group').each(function(index, group) {
+        // Loop over inputs to see if any one is selected
+        var inputs = $(group).find('input');
+        var hasValue = false;
         
-        var totalQuestions = $('#surveyQuestions .form-group').length;
-        var totalAnswered = 0;
-        
-        // Loop over all our questions if a value is entered for any of our fields.
-        // mark it as an asnwered question
-        $('#surveyQuestions .form-group').each(function(index, group) {
-            // Loop over inputs to see if any one is selected
-            var inputs = $(group).find('input');
-            var hasValue = false;
+        for (var x = 0; x < inputs.length; x++) {
+            var ele = $(inputs[x]);
             
-            for (var x = 0; x < inputs.length; x++) {
-                var ele = $(inputs[x]);
-                
-                // Test input to see if it has a value or is selected
-                switch (ele.attr('type')) {
-                    case "checkbox":
-                    case "radio":
-                      if (ele.is(":checked")) {
+            // Test input to see if it has a value or is selected
+            switch (ele.attr('type')) {
+                case "checkbox":
+                case "radio":
+                  if (ele.is(":checked")) {
+                    hasValue = true;
+                  };
+                  break;
+                default:
+                    if (ele.val()) {
                         hasValue = true;
-                      };
-                      break;
-                    default:
-                        if (ele.val()) {
-                            hasValue = true;
-                        }
-                        break;
-                }
-                
-                if (hasValue) {
-                    totalAnswered++
+                    }
                     break;
-                }
             }
-        });
-        
-        var percentComplete = parseInt( (totalAnswered / totalQuestions) * 100 );
-        
-        // Loop over the progress labels and set the ones where value <= percentComplete
-        container.find('.progress-labels div').each(function(key, ele){
-            var ele = $(ele);
             
-            ele.toggleClass('completed',parseFloat(ele.data('value')) <= percentComplete);
-        });
+            if (hasValue) {
+                totalAnswered++
+                break;
+            }
+        }
+    });
+    
+    var percentComplete = parseInt( (totalAnswered / totalQuestions) * 100 );
+    
+    // Loop over the progress labels and set the ones where value <= percentComplete
+    container.find('.progress-labels div').each(function(key, ele){
+        var ele = $(ele);
         
-        container.find('.progress-bar').css('width', percentComplete+'%').attr('aria-valuenow', percentComplete);    
-        
-    }
+        ele.toggleClass('completed',parseFloat(ele.data('value')) <= percentComplete);
+    });
+    
+    container.find('.progress-bar').css('width', percentComplete+'%').attr('aria-valuenow', percentComplete);    
+    
+}
